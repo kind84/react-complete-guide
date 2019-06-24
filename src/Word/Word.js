6b4319e41e16a7a1870a58b5e01aa59a5aaefa0c
Paolo Grisoli <paolo_g84@icloud.com>
2019-06-24 16:42:49 +0200

    Using css modules

--- src/Word/Word.js
+++ src/Word/Word.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import styles from './Word.module.css'
 
 const Word = props => {
   let text = null
+  let style = null
   if (props.wordLen > 0 && props.wordLen < 5) {
     text = "Word too short"
+    style = styles.red
   } else if (props.wordLen >= 5) {
     text = "Word long enough"
+    style = styles.green
   }
   
   return (
     <div>
-      <p>{text}</p>
+      <p className={style}>{text}</p>
     </div>
   )
 }
